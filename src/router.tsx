1013174c5a35f2fa56198ba6a53a440885415b44
Jonnathan23 <jsaquicela26@gmail.com>
2024-12-08 22:42:00 -0500

    manejo de rutas y formulario creado

--- src/router.tsx
+++ src/router.tsx
@@ -1,20 +1,21 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Layout from './layouts/Layout'
 import Products from './pages/Products'
-import NewProduct from './pages/NewProduct'
+import NewProduct, { action as newProductAction} from './pages/NewProduct'
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <Layout />,
-        children:[
+        children: [
             {
                 index: true,
                 element: <Products />
             },
             {
-                path: '/products/new',
-                element: <NewProduct />
+                path: 'productos/nuevo',
+                element: <NewProduct />,
+                action: newProductAction
             }
         ]
     }
